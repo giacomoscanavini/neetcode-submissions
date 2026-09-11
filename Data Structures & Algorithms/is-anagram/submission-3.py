31d2c7a5452886a1d57acebb7322e004297b2a78
giacomoscanavini <37457879+giacomoscanavini@users.noreply.github.com>
2026-09-11 14:22:17 +0800

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Complexity Time: O(n) to traverse s and t
+        # Complexity Memory: O(n) to create a hash table
+        if len(s) != len(t): return False
+        
+        hash_s = {}
+        hash_t = {}
+
+        for char in s: 
+            hash_s[char] = hash_s.get(char, 0) + 1
+
+        for char in t:
+            hash_t[char] = hash_t.get(char, 0) + 1
+
+        keys = list(hash_s.keys()) + list(hash_t.keys())
+
+        for key in keys:
+            if hash_s.get(key, 0) != hash_t.get(key, 0): return False
+        
+        return True
+
+
+        
