05ba0d1893ac1fdc060d2ea2961be6f4b17f761f
giacomoscanavini <37457879+giacomoscanavini@users.noreply.github.com>
2026-09-13 20:18:01 +0800

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Complexity Time: 
+        # Complexity Memory: 
+        hash_map = {}
+        for num in nums:
+            hash_map[num] = hash_map.get(num, 0) + 1
+        
+        # Using bucket sort
+        buckets = [None] * (len(nums) + 1)
+        for num,freq in hash_map.items():
+            if buckets[freq] is None: 
+                buckets[freq] = [num]
+            else:
+                buckets[freq].append(num)
+
+        top_k = []
+        for bucket in buckets[::-1]:
+            if bucket is None: 
+                pass
+            else:
+                top_k += bucket
+                if len(top_k) == k: return top_k
